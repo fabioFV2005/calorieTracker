1458bf201f01275f9074fe27015a5949b8c79e14
Fabio Fernandez <kingofnoobs845@gmail.com>
2025-12-02 15:55:35 -0400

    correccion grafico

--- src/components/DiferenceDisplay.tsx
+++ src/components/DiferenceDisplay.tsx
@@ -12,12 +12,11 @@ export default function DiferenceDisplay({caloriesBurned, caloriesConsumed }: Di
                 data: [
                     { id: 0, value: caloriesBurned, color: '#f97316', },
                     { id: 1, value: caloriesConsumed, color: '#22c55e'},
-                ],
+                    ],
                 },
             ]}
-            
-            width={200}
-            height={200}
+            width={130}
+            height={130}
             />
   )
 }
